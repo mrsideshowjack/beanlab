11980741089b0f9a518772cc687e97a2a473c0a1
Jack Mason <mrsideshowjack@gmail.com>
2025-07-24 01:00:13 +0900

    cloudflared

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -3,5 +3,18 @@
 {
   services.cloudflared = {
     enable = true;
+    package = pkgs.cloudflared;
+    user = "bean";
+    group = "users";
+    origincert = "/home/bean/.cloudflared/cert.pem";
+    tunnels = {
+      "851dd3ff-a13f-4db7-a23a-7c366b9567e4" = {
+        credentialsFile = "/home/bean/.cloudflared/851dd3ff-a13f-4db7-a23a-7c366b9567e4.json";
+        default = "http_status:404";
+        ingress = {
+          "beanlab.jack-mason.dev" = "http://localhost:3000";
+        };
+      };
+    };
   };
 }

91f1b76047f32fd18e757e9d13b03f36b769b95a
Jack Mason <mrsideshowjack@gmail.com>
2025-07-24 01:04:21 +0900

    remove user

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -4,8 +4,6 @@
   services.cloudflared = {
     enable = true;
     package = pkgs.cloudflared;
-    user = "bean";
-    group = "users";
     tunnels = {
       "851dd3ff-a13f-4db7-a23a-7c366b9567e4" = {
         credentialsFile = "/home/bean/.cloudflared/851dd3ff-a13f-4db7-a23a-7c366b9567e4.json";
